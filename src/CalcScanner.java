deb5c6bc4937c6246db44c3ccfd1437bc36df075
mpsolle <denopi@gmail.com>
2013-09-12 17:25:28 -0400

    alot of bugs but some testing working

--- src/CalcScanner.java
+++ src/CalcScanner.java
@@ -19,20 +19,37 @@ public class CalcScanner {
         File file = new File(args[0]);
        // System.out.print(file);
         PushbackReader in = new PushbackReader(new FileReader(file));
-        CalcScanner scan = new CalcScanner();
-    	
-        
+        CalcScanner scan = new CalcScanner();        
         int s;
         String result = "";
         do {
         	s = scan.nextToken(in);
         	char token;
         	token = (char) s;       	
-        	if(String.valueOf(token).matches("^[a-zA-Z]") ) {
-        		result+=token;
-        	}
-        	System.out.print(result);
+        	
+        		if(String.valueOf(token).matches("^[a-zA-Z]") ) {
+        			if(String.valueOf(token).matches("[1-9]")) {
+        				result+=token;
+        			} else result+=token;
+        		}// a-z
+        		
+        		if(token == '('
+        				|| token == ')' 
+        				|| token == '+'
+        				|| token == '-'
+        				|| token == '*'
+        				|| token == '/') {
+        			result+=token;
+        		}//ids
+        		
+        		if(token == '$') {
+        			if(scan.nextToken(in) == '$') {
+        				break;
+        			} else result+=" error here ";
+        		}
+        		
         } while(s != -1);
+    	System.out.print(result);
     } else {
     	System.out.print("There needs to be a file specified when launching. Format: -java CalcScanner [filename]");
     }
